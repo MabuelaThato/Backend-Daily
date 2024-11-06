77deab777392793ed2f260ebcaa32e49fc61b0b3
Thato Mabuela <mabuelathato03@gmail.com>
2024-11-06 20:24:07 +0200

    Add code to create letters

--- mail merge/main.py
+++ mail merge/main.py
@@ -1,8 +1,38 @@
-#TODO: Create a letter using starting_letter.docx 
-#for each name in invited_names.txt
-#Replace the [name] placeholder with the actual name.
-#Save the letters in the folder "ReadyToSend".
-    
-#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+import os
+def get_names():
+    try:
+        with open("mail merge/Input/Names/inivited_names.txt","r") as file:
+            names_list = file.readlines()
+            return names_list
+    except Exception:
+        print("Failed to open names file")
+
+
+def main():
+    try:
+        with open("mail merge/Input/letters/starting_letter.docx", 'r') as file:
+            text = file.readlines()
+    except Exception:
+        print("Failed to open starting letter")
+
+    directory_name = "ReadyToSend"
+
+    try:
+        os.mkdir(f"mail merge/Input/letters/{directory_name}")
+    except Exception:
+        print("Failed to create directory")
+
+    names = get_names()
+    for name in names:
+        name = name.replace("\n","")
+        try:
+            with open(f"mail merge/Input/letters/ReadyToSend/{name}.txt","w") as letter:
+                for line in text:
+                    line = line.replace("[name]", name)
+                    if len(line) > 2:
+                        line = line.replace("\n","")
+                    letter.write(line)
+        except Exception:
+            print("Failed to write new letter")
+if __name__ == "__main__":
+    main()
